ca590465b019e117078871a81884890ed993b74f
Ronal2 <ronal2.gutierrez@gmail.com>
2020-06-28 15:52:53 -0500

    Base server

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.json({
+        menssage: "working good"
+    })
+})
+
+app.listen(port, () => {
+    console.log(`servidor corriendo en el puerto ${port}`)
+})
